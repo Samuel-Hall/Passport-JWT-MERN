504c9c012753a58ad0197a74e495c046322a0d11
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-11-05 14:29:17 -0500

    azure deploy

--- database/mongo.js
+++ database/mongo.js
@@ -1,27 +1,20 @@
 //Connect to Mongo database
 const mongoose = require("mongoose");
+const env = require("../environment");
 mongoose.Promise = global.Promise;
 
-//your local database url
-//27017 is the default mongoDB port
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/Passport-JWT-MERN_DB";
+const mongoUri = `mongodb://${env.dbName}:${env.key}@${
+  env.dbName
+}.documents.azure.com:${env.port}/?ssl=true`;
 
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect(uri).then(
-    () => {
-      /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
-  
-      console.log("Connected to Mongo");
-    },
-    err => {
-      /** handle initial connection error */
-  
-      console.log("error connecting to Mongo: ");
-      console.log(err);
-    }
+function connect() {
+  return mongoose.connect(
+    mongoUri,
+    { useMongoClient: true }
   );
 }
 
-module.exports = mongoose.connection;
+module.exports = {
+  connect,
+  mongoose
+};
